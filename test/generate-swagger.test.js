4fc33b117f1b6256dbe29bdf73660fa1001d1607
Venkat Balachandran <venkat.balachandran@luxuryescapes.com>
2024-08-21 16:54:11 +1000

    add token

--- test/generate-swagger.test.js
+++ test/generate-swagger.test.js
@@ -99,7 +99,7 @@ describe('generateSwagger', () => {
           name: 'Cookie',
           in: 'header',
           description: 'Cookie',
-          scheme: 'PrivateToken'
+          scheme: 'token'
         }
       }
     )
